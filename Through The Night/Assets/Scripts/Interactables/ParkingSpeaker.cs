2d320cb28fc1b63b8b985828f675d499e7998f23
Lucas-Burford <89630296+LucasBurford@users.noreply.github.com>
2022-01-23 17:33:11 +0000

    Added to level

---
+++ Through The Night/Assets/Scripts/Interactables/ParkingSpeaker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkingSpeaker : MonoBehaviour
+{
+    public Animator animator;
+
+    public void OnInteract()
+    {
+        animator.SetBool("Open", true);
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(2);
+        animator.SetBool("Open", false);
+    }
+}
