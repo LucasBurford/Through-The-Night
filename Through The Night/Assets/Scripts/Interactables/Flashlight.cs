e12b1ab9d6e1e9c5dc03a2b4c3b1118baed7a009
Lucas-Burford <89630296+LucasBurford@users.noreply.github.com>
2022-01-23 11:11:19 +0000

    Added flashlight item

---
+++ Through The Night/Assets/Scripts/Interactables/Flashlight.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light mLight;
+
+    public float energy;
+    public float maxEnergy;
+    public float energyRate;
+
+    public bool state;
+    public bool shouldIncrease;
+    public bool shouldDecrease;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!state)
+            {
+                state = true;
+            }
+            else if (state)
+            {
+                state = false;
+            }
+        }
+
+        if (state)
+        {
+            shouldDecrease = true;
+            shouldIncrease = false;
+        }
+        else if (!state)
+        {
+            shouldDecrease = false;
+            shouldIncrease = true;
+        }
+
+        if (energy <= 0)
+        {
+            energy = 0;
+            state = false;
+            shouldIncrease = true;
+            shouldDecrease = false;
+        }
+        else if (energy >= maxEnergy)
+        {
+            energy = maxEnergy;
+            shouldIncrease = false;
+            shouldDecrease = true;
+        }
+
+        if (shouldIncrease)
+        {
+            energy += energyRate;
+        }
+        if (shouldDecrease)
+        {
+            energy -= energyRate;
+        }
+
+        mLight.GetComponent<Light>().enabled = state;
+    }
+}
---
+++ Through The Night/Assets/Scripts/Interactables/PickUpController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpController : MonoBehaviour
+{
+    public Flashlight flashlight;
+    public Rigidbody rb;
+    public BoxCollider col;
+    public Transform player;
+    public Transform flaslightContainer;
+    public Transform fpsCam;
+
+    public float pickUpRange;
+    public float dropForwardForce;
+    public float dropUpwardForce;
+
+    public bool equipped;
+    public static bool slotFull;
+
+    private void Start()
+    {
+        if (!equipped)
+        {
+            flashlight.enabled = false;
+            rb.isKinematic = false;
+            col.isTrigger = false;
+        }
+        if (equipped)
+        {
+            flashlight.enabled = true;
+            rb.isKinematic = true;
+            col.isTrigger = true;
+            slotFull = true;
+        }
+    }
+
+    private void Update()
+    {
+        // Check if player is in range and presses E
+        Vector3 distanceToPlayer = player.position - transform.position;
+
+        if (!equipped && distanceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.E) && !slotFull)
+        {
+            Pickup();
+        }
+
+        // Drop is pressed Q
+        if (equipped && Input.GetKeyDown(KeyCode.Q))
+        {
+            Drop();
+        }
+    }
+
+    private void Pickup()
+    {
+        equipped = true;
+        slotFull = true;
+
+        transform.SetParent(flaslightContainer);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(90, 0, 0);
+        transform.localScale = new Vector3(5, 5, 5);
+
+        rb.isKinematic = true;
+        col.isTrigger = true;
+
+        flashlight.enabled = true;
+    }
+
+    private void Drop()
+    {
+        equipped = false;
+        slotFull = false;
+
+        transform.SetParent(null);
+
+        rb.isKinematic = false;
+        col.isTrigger = false;
+
+        rb.velocity = player.GetComponent<Rigidbody>().velocity;
+
+        rb.AddForce(fpsCam.forward * dropForwardForce, ForceMode.Impulse);
+        rb.AddForce(fpsCam.forward * dropUpwardForce, ForceMode.Impulse);
+
+        flashlight.enabled = false;
+    }
+}
