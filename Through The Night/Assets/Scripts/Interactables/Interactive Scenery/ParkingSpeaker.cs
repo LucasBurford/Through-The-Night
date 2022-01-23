11bddbbd9a34913e797612c98e0f2689234a0c46
Lucas-Burford <89630296+LucasBurford@users.noreply.github.com>
2022-01-23 20:28:25 +0000

    Added lots of scenery

---
+++ Through The Night/Assets/Scripts/Interactables/Interactive Scenery/ParkingSpeaker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkingSpeaker : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject go;
+
+    public void OnInteract()
+    {
+        go.transform.position = new Vector3(-0.06f, 0.87f, 0.24f);
+        go.transform.rotation = Quaternion.Euler(0, 0, -40);
+    }
+}
